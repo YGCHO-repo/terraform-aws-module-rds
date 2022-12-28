aa21229420706d9136167d982f90df9dc296479c
Steve ChoCho <cyk3233@gmail.com>
2022-12-29 01:34:16 +0900

    commit

--- subnet_group.tf
+++ subnet_group.tf
@@ -1,11 +1,4 @@
 resource "aws_db_subnet_group" "this" {
-<<<<<<< HEAD
   subnet_ids = data.aws_subnets.selected_subnet.ids
   name       = "${var.prefix}-rds-subnet-group"
 }
-=======
-    subnet_ids = data.aws_subnets.selected_subnet.ids
-    name = "${var.prefix}-rds-subnet-group"
-    
-}
->>>>>>> 5ff9e3f034ddfbaa06426fdad7bf1b43c3e9b5de
